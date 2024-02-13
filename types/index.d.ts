1b22589741069bcd26777e39442da485ebf77c66
Yurii Nikitiuk <40966744+ynikitiuk@users.noreply.github.com>
2024-02-13 12:35:22 +0200

    Feature/extend render props type (#12)

--- types/index.d.ts
+++ types/index.d.ts
@@ -11,9 +11,15 @@ export interface GuestyTokenizationStyles {
   colorBorderError?: string;
   colorBorderHover?: string;
   colorPlaceholder?: string;
+  colorFormBackground?: string;
   colorBackgroundError?: string;
   colorBackgroundDisabled?: string;
+  inputHeight?: number;
+  inputPadding?: number;
+  borderRadius?: number;
 }
+
+type Section = 'cardholderName' | 'paymentDetails' | 'billingAddress';
 
 export interface GuestyTokenizationRenderOptions {
   containerId: string;
@@ -23,6 +29,20 @@ export interface GuestyTokenizationRenderOptions {
   onStatusChange?: (status: boolean) => void;
   styles?: GuestyTokenizationStyles;
   lang?: string;
+  initialValues?: {
+    firstName?: string;
+    lastName?: string;
+    street?: string;
+    city?: string;
+    state?: string;
+    zipCode?: string;
+    country?: string;
+  };
+  showInitialValuesToggle?: boolean;
+  initialValuesToggleLabel?: string;
+  sections?: Section[];
+  showSupportedCards?: boolean;
+  showPciCompliantLink?: boolean;
 }
 
 export interface PaymentMethod {
